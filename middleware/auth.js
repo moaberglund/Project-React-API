090311e1627676095c31ce01d7a2f04469630523
moaberglund <moa.21@hotmail.com>
2025-03-06 14:25:02 +0100

    Enhance authentication middleware to fetch full User data from database after token verification

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,16 +1,29 @@
 const jwt = require('jsonwebtoken');
+const User = require('../models/UserModel'); 
 
-module.exports = (req, res, next) => {
+module.exports = async (req, res, next) => {
     const token = req.header("Authorization");
-    if(!token) {
-        return res.status(401).json({message: "Access denied!"});
+    if (!token) {
+        return res.status(401).json({ message: "Access denied!" });
     }
 
     try {
         const decoded = jwt.verify(token.replace("Bearer ", ""), process.env.JWT_SECRET_KEY);
-        req.user = decoded; // store decoded data in request object
-        next();
+
+        req.user = decoded; // Add decoded data in request objectet
+
+        // Fetch User from database based on decoded userId
+        const user = await User.findById(decoded.userId);
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        req.user = user;  // Add full User in request object
+
+        next(); // Continue to next middleware
+
     } catch (err) {
-        res.status(401).json({message: "Invalid token"});
+        console.log("Error decoding token:", err);
+        res.status(401).json({ message: "Invalid token" });
     }
 };
