be1cbc37ad69f32eb154ca813ddfb1a03d797994
moaberglund <moa.21@hotmail.com>
2025-03-10 17:35:41 +0100

    Add route to retrieve the status of a specific book from the user's bookshelf

--- routes/BookShelfRoutes.js
+++ routes/BookShelfRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { getBooksFromShelf, addBookToShelf, updateBookStatus, removeBookFromShelf } = require('../controllers/BookShelfController');
+const { getBooksFromShelf, getBookStatusFromShelf, addBookToShelf, updateBookStatus, removeBookFromShelf } = require('../controllers/BookShelfController');
 const auth = require('../middleware/auth');
 
 router.get('/', auth, getBooksFromShelf);
+router.get('/:book_id', auth, getBookStatusFromShelf);
 router.post('/', auth, addBookToShelf);  
 router.put('/:book_id', auth, updateBookStatus); 
 router.delete('/:book_id', auth, removeBookFromShelf); 
